2c24e78c2357d514f4036d0af24ce71854507b0e
Mateusz <mateusz.jajkowicz@gmail.com>
2023-01-29 14:34:03 +0100

    Added proxy middleware

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,8 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = {
+  target: process.env.REACT_APP_IP,
+  changeOrigin: true,
+};
+module.exports = function (app) {
+  app.use('/api', createProxyMiddleware(proxy));
+};
