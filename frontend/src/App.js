337fa480a663e207ec37fdb4be0048851eea0e96
Mateusz <mateusz.jajkowicz@gmail.com>
2023-02-06 14:16:46 +0100

    Added PlaceOrderScreen on FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Route path='/cart/:id?' element={<CartScreen />} />
             <Route path='/shipping' element={<ShippingScreen />} />
             <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
             <Route path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
