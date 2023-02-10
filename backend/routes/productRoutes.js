4a26477676834db4df628fa176defe82069ee212
Mateusz <mateusz.jajkowicz@gmail.com>
2023-02-10 13:47:23 +0100

    Added createProduct, updateProduct in controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,16 +3,19 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
 import { isAdmin, protect } from '../middlewere/authMiddlewere.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
 
 router
   .route('/:id')
   .get(getProductById)
-  .delete(protect, isAdmin, deleteProduct);
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
