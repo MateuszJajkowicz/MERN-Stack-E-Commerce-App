c94e6373a39af033fb38a3e0e79ca40b8bc6bb1f
Mateusz <mateusz.jajkowicz@gmail.com>
2023-02-14 22:43:54 +0100

    Create reviews endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -69,22 +69,61 @@ const updateProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    product.name = name
-    product.price = price
-    product.description = description
-    product.image = image
-    product.brand = brand
-    product.category = category
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
     product.countInStock = countInStock;
 
     const updatedProduct = await product.save();
     res.json(updatedProduct);
-
   } else {
     res.status(404);
     throw new Error('Product not found');
   }
+});
 
+// @desc    Create new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => acc + item.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: 'Review added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
 });
 
 export {
@@ -93,4 +132,5 @@ export {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,13 +5,14 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { isAdmin, protect } from '../middlewere/authMiddlewere.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
-
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct) ;
+router.route('/:id/reviews').post(protect, createProductReview);
 router
   .route('/:id')
   .get(getProductById)
