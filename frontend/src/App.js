30806e939390202f754c0f8699739b6ce0be111f
Mateusz <mateusz.jajkowicz@gmail.com>
2023-03-04 15:02:45 +0100

    Added ScrollToTop on every tranistion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense, useLayoutEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -21,48 +26,61 @@ const OrdersListScreen = lazy(() => import('./screens/OrdersListScreen'));
 const ProductEditScreen = lazy(() => import('./screens/ProductEditScreen'));
 const HomeScreen = lazy(() => import('./screens/HomeScreen'));
 
+const ScrollToTop = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
+
 function App() {
   return (
     <Router>
       <Header></Header>
       <main className='py-3'>
-        <Container>
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path='/login' element={<LoginScreen />} />
-              <Route path='/register' element={<RegisterScreen />} />
-              <Route path='/profile' element={<ProfileScreen />} />
-              <Route path='/product/:id' element={<ProductScreen />} />
-              <Route path='/cart/:id?' element={<CartScreen />} />
-              <Route path='/shipping' element={<ShippingScreen />} />
-              <Route path='/payment' element={<PaymentScreen />} />
-              <Route path='/placeorder' element={<PlaceOrderScreen />} />
-              <Route path='/order/:id' element={<OrderScreen />} />
-              <Route path='/admin/userlist' element={<UsersListScreen />} />
-              <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
-              <Route
-                path='/admin/productlist'
-                element={<ProductsListScreen />}
-              />
-              <Route
-                path='/admin/productlist/:pageNumber'
-                element={<ProductsListScreen />}
-              />
-              <Route path='/admin/orderlist' element={<OrdersListScreen />} />
-              <Route
-                path='/admin/product/:id/edit'
-                element={<ProductEditScreen />}
-              />
-              <Route path='/search/:keyword' element={<HomeScreen />} />
-              <Route path='/page/:pageNumber' element={<HomeScreen />} />
-              <Route
-                path='/search/:keyword/page/:pageNumber'
-                element={<HomeScreen />}
-              />
-              <Route path='/' element={<HomeScreen />} />
-            </Routes>
-          </Suspense>
-        </Container>
+        <ScrollToTop>
+          <Container>
+            <Suspense fallback={<Loader />}>
+              <Routes>
+                <Route path='/login' element={<LoginScreen />} />
+                <Route path='/register' element={<RegisterScreen />} />
+                <Route path='/profile' element={<ProfileScreen />} />
+                <Route path='/product/:id' element={<ProductScreen />} />
+                <Route path='/cart/:id?' element={<CartScreen />} />
+                <Route path='/shipping' element={<ShippingScreen />} />
+                <Route path='/payment' element={<PaymentScreen />} />
+                <Route path='/placeorder' element={<PlaceOrderScreen />} />
+                <Route path='/order/:id' element={<OrderScreen />} />
+                <Route path='/admin/userlist' element={<UsersListScreen />} />
+                <Route
+                  path='/admin/user/:id/edit'
+                  element={<UserEditScreen />}
+                />
+                <Route
+                  path='/admin/productlist'
+                  element={<ProductsListScreen />}
+                />
+                <Route
+                  path='/admin/productlist/:pageNumber'
+                  element={<ProductsListScreen />}
+                />
+                <Route path='/admin/orderlist' element={<OrdersListScreen />} />
+                <Route
+                  path='/admin/product/:id/edit'
+                  element={<ProductEditScreen />}
+                />
+                <Route path='/search/:keyword' element={<HomeScreen />} />
+                <Route path='/page/:pageNumber' element={<HomeScreen />} />
+                <Route
+                  path='/search/:keyword/page/:pageNumber'
+                  element={<HomeScreen />}
+                />
+                <Route path='/' element={<HomeScreen />} />
+              </Routes>
+            </Suspense>
+          </Container>
+        </ScrollToTop>
       </main>
       <Footer></Footer>
     </Router>
